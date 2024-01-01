54fad3b0a736b31fa6193da5f80b9a532fa548b1
Mohit-Pala <palav@appstate.edu>
2024-01-01 11:14:48 -0500

    added run

--- Game/Scripts/Global Variables/run.gd
+++ Game/Scripts/Global Variables/run.gd
@@ -1,11 +1,24 @@
 extends Node
 
+# main stats
+var charHealth = 10
+var enemiesKilled = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# char pos
+var charPosx = 0
+var charPosy = 0
 
+# is char alive
+var alive = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# main stats
+func hit():
+	charHealth -= 1
+
+func killed():
+	enemiesKilled += 1
+
+# change char pos
+func changeCharPos(x, y):
+	charPosx = x
+	charPosy = y

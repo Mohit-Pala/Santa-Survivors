fcf71260fade8647600da6d8ffa7966bf87e3ac4
Mohit-Pala <palav@appstate.edu>
2024-01-01 20:29:10 -0500

    finished cheat menu

--- Game/Scripts/Global Variables/gold.gd
+++ Game/Scripts/Global Variables/gold.gd
@@ -5,5 +5,8 @@ var gold = 0
 func reduceGold(amount):
 	gold -= amount
 
+func addGold(amount):
+	gold += amount
+
 func changeGold(amount):
 	gold = amount
--- Game/Scripts/Ui/Cheat Menu.gd
+++ Game/Scripts/Ui/Cheat Menu.gd
@@ -1,6 +1,36 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	updateGold()
+
+func updateGold():
+	$"Gold label".text = "Gold:\n" + str(Gold.gold)
+
+func _on_hundred_pressed():
+	Gold.addGold(100)
+	updateGold()
+
+func _on_five_hundred_pressed():
+	Gold.addGold(500)
+	updateGold()
+
+func _on_thousand_pressed():
+	Gold.addGold(1000)
+	updateGold()
+
+func _on_ten_thousand_pressed():
+	Gold.addGold(10000)
+	updateGold()
+
+func _on_fifty_thousand_pressed():
+	Gold.addGold(50000)
+	updateGold()
+
+func _on_hundred_thousand_pressed():
+	Gold.addGold(100000)
+	updateGold()
+
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://Game/Scenes/Ui/Main Menu.tscn")

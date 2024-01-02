1f0e4414938529108f7d034dbdf141937eddefea
Mohit-Pala <palav@appstate.edu>
2024-01-02 09:17:39 -0500

    Added nodes

--- Game/Scripts/Ui/Upgrades.gd
+++ Game/Scripts/Ui/Upgrades.gd
@@ -13,3 +13,47 @@ func updateButtons():
 func updateLabels():
 	$"Gold label".text = "Gold\n" + str(Gold.gold)
 	$"Upgrades label".text = "Upgrades: " + str(Upgrades.upgrades) + "\nLimit Break: " + str(Upgrades.limitBreak)
+
+
+func _on_health_pressed():
+	pass # Replace with function body.
+
+
+func _on_regen_pressed():
+	pass # Replace with function body.
+
+
+func _on_curse_pressed():
+	pass # Replace with function body.
+
+
+func _on_amount_pressed():
+	pass # Replace with function body.
+
+
+func _on_speed_pressed():
+	pass # Replace with function body.
+
+
+func _on_greed_pressed():
+	pass # Replace with function body.
+
+
+func _on_projectile_speed_pressed():
+	pass # Replace with function body.
+
+
+func _on_projectile_timeout_pressed():
+	pass # Replace with function body.
+
+
+func _on_limit_break_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_to_main_menu_pressed():
+	pass # Replace with function body.
+
+
+func _on_to_weapon_shop_pressed():
+	pass # Replace with function body.

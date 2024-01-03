20a7b7a5bb421f56dcbc3be076a2296e4fe0c5dc
Mohit-Pala <palav@appstate.edu>
2024-01-03 11:07:58 -0500

    added basic enemy

---
+++ Game/Scripts/Enemies/Snowman.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _physics_process(delta):
+	position.x = move_toward(self.position.x, Run.charPosx, delta * (30 + (Upgrades.curse * 2)))
+	position.y = move_toward(self.position.y, Run.charPosy, delta * (30 + (Upgrades.curse * 2)))
+
+func _on_body_entered(body):
+	if(body.is_in_group("Player")):
+		Run.hit()
+		self.queue_free()
+	elif(body.is_in_group("Projectile")):
+		Run.killed()
+		self.queue_free()
